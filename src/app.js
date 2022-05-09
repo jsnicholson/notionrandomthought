0f8f42f2288ba5d2427be18f52817d866ca7d457
Josh Nicholson <jonic@netcompany.com>
2022-05-09 09:11:01 +0100

    make paths clearer

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const vue = require('https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js');
 
-import Config from "Config.js";
-import FindAllAvailableDb from "FindAllAvailableDb.js";
-import FetchAllForgottenItemsFromAllSources from "FetchAllForgottenItemsFromAllSources.js";
-import FetchAllUnwantedItemsFromDestination from "FetchAllUnwantedItemsFromDestination.js";
-import RemoveUnwantedItemsFromSourceItems from "RemoveUnwantedItemsFromSourceItems";
-import PickRandomItems from "PickRandomItems.js";
-import SimplifyItems from "SimplifyItems.js";
-import ClearWantedItemsFromDest from "ClearWantedItemsFromDest.js";
-import PushAllSimpleItemsToSimpleDb from "PushAllSimpleItemsToSimpleDb.js";
+import Config from "/steps/Config.js";
+import FindAllAvailableDb from "/steps/FindAllAvailableDb.js";
+import FetchAllForgottenItemsFromAllSources from "/steps/FetchAllForgottenItemsFromAllSources.js";
+import FetchAllUnwantedItemsFromDestination from "/steps/FetchAllUnwantedItemsFromDestination.js";
+import RemoveUnwantedItemsFromSourceItems from "/steps/RemoveUnwantedItemsFromSourceItems";
+import PickRandomItems from "/steps/PickRandomItems.js";
+import SimplifyItems from "/steps/SimplifyItems.js";
+import ClearWantedItemsFromDest from "/steps/ClearWantedItemsFromDest.js";
+import PushAllSimpleItemsToSimpleDb from "/steps/PushAllSimpleItemsToSimpleDb.js";
 
 let vm = new vue({
     $:{},
@@ -28,16 +28,16 @@ let vm = new vue({
         handle:this.Handler(e)
     },
     methods: {
-        async Handle(e) {
-            Config(steps,$);
-            FindAllAvailableDb(steps,$);
-            FetchAllForgottenItemsFromAllSources(steps,$);
-            FetchAllUnwantedItemsFromDestination(steps,$);
-            RemoveUnwantedItemsFromSourceItems(steps,$);
-            PickRandomItems(steps,$);
-            SimplifyItems(steps,$);
-            ClearUnwantedItemsFromDest(steps,$);
-            PushAllSimpleItemsToSimpleDb(steps,$);
+        async Handler(e) {
+            Config(this.steps,this.$);
+            FindAllAvailableDb(this.steps,this.$);
+            FetchAllForgottenItemsFromAllSources(this.steps,this.$);
+            FetchAllUnwantedItemsFromDestination(this.steps,this.$);
+            RemoveUnwantedItemsFromSourceItems(this.steps,this.$);
+            PickRandomItems(this.steps,this.$);
+            SimplifyItems(this.steps,this.$);
+            ClearUnwantedItemsFromDest(this.steps,this.$);
+            PushAllSimpleItemsToSimpleDb(this.steps,this.$);
         }
     }
 });
