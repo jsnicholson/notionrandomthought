2e69932b54a8ac34b72d518b13f66727e772502f
Josh Nicholson <jonic@netcompany.com>
2022-05-25 14:07:04 +0100

    sam template for lambda function

--- src/steps/trigger.js
+++
@@ -1,9 +0,0 @@
-const cron = require('node-cron');
-
-export default defineComponent({
-    async run({steps,$}){
-        cron.schedule('0 2 * * *', () => {
-            this.$emit("WorkflowStarted",{ "time_start":(new Date().toISOString()) });
-        });
-    }
-});
