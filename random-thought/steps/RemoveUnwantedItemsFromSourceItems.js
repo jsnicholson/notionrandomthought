f830f56df6491c0b1b4adceb539ba42b272122d8
Josh Nicholson <jsnicholson08@gmail.com>
2022-06-30 23:29:43 +0100

    changed to not filter in place and actually filter out unwanted items

--- random-thought/steps/RemoveUnwantedItemsFromSourceItems.js
+++ random-thought/steps/RemoveUnwantedItemsFromSourceItems.js
@@ -1,20 +1,16 @@
 function run(sourceAllItems, destinationUnwantedItems) {
     console.log("removing unwanted items from source items");
-    let sourceWantedItems = sourceAllItems;
-    for(const unwanted of destinationUnwantedItems)
-        FilterInPlace(sourceWantedItems, item => item.properties.original_id != unwanted.id);
-    return sourceWantedItems;
-}
-
-function FilterInPlace(array, condition) {
-    let i = 0, j = 0;
-    while(i < array.length) {
-        const val = array[i];
-        if(condition(val, i, array)) array[j++] = val;
-        i++;
+    let sourceWantedItems = [];
+    for(const item of sourceAllItems) {
+        let unwanted = false;
+        for(const unwantedItem of destinationUnwantedItems) {
+            if(item.id == unwantedItem.properties.original_id.rich_text[0].text.content)
+                unwanted = true;
+        }
+        if(!unwanted)
+            sourceWantedItems.push(item);
     }
-    array.length = j;
-    return array;
+    return sourceWantedItems;
 }
 
 module.exports = {run};
